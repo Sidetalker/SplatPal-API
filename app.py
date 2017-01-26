fab58e1d113bea845af03f800ce5086af82e8daa
Kevin Sullivan <kevin@Kevins-MacBook-Pro-2.local>
2017-01-26 18:48:07 -0500

    Stuff from 3 days ago idk

--- app.py
+++ app.py
@@ -19,9 +19,12 @@ app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'this_should_be_configur
 ###
 
 @app.route('/')
-def home():
-    """Render website's home page."""
-    return render_template('home.html')
+def index():
+    """GET to generate a list of endpoints and their docstrings"""
+    urls = dict([(r.rule, app.view_functions.get(r.endpoint).func_doc)
+                 for r in app.url_map.iter_rules()
+                 if not r.rule.startswith('/static')])
+    return render_template('index.html', urls=urls)
 
 
 @app.route('/about/')
@@ -29,16 +32,16 @@ def about():
     """Render the website's about page."""
     return render_template('about.html')
 
+@app.route('/get/<thing>', methods=['GET'])
+def select(thing):
+    """Retrieve a thing.
 
-###
-# The functions below should be applicable to all Flask apps.
-###
-
-@app.route('/<file_name>.txt')
-def send_text_file(file_name):
-    """Send your static text file."""
-    file_dot_text = file_name + '.txt'
-    return app.send_static_file(file_dot_text)
+    GET: Returns thing previously 'put' at given location.
+         Returns HTTP 200 on success; body is payload as-is.
+         Returns HTTP 404 when data does not exist.
+    """
+    # result = db.get(thing)
+    return 1
 
 
 @app.after_request
