d067c7c521edcd90e67b2dc35911eceb74bceb9b
Andrew <andrew.dorman.taylor@gmail.com>
2017-02-11 13:40:19 -0500

    Create helper service method for constructing optional queries

--- splatter.py
+++ splatter.py
@@ -15,11 +15,13 @@ class SplatService(object):
         result = self.database.APIKeys.insert_one({'name': name, 'key': key})
         return str(result.inserted_id)
 
-    def delete_api_key(self, doc_id=-1, name=None, key=None):
-        query_filter = {'_id': doc_id}
-        if name is not None:
-            query_filter['name'] = name
-        if key is not None:
-            query_filter['key'] = key
+    def _construct_optional_query(self, dic):
+        query_filter = {}
+        for key, value in dic.items():
+            if value is not None:
+                query_filter[key] = value
+        return query_filter
 
+    def delete_api_key(self, doc_id=-1, name=None, key=None):
+        query_filter = self._construct_optional_query({'_id': doc_id, 'name': name, 'key': key})
         return self.database.APIKeys.find_one_and_delete(query_filter)
