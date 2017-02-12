f22a52aafd53cfaf87b7212ae333636dfafb184a
Andrew <andrew.dorman.taylor@gmail.com>
2017-02-11 19:38:12 -0500

    Round-out the CRUD for api keys, now sporting tests!

--- splatter.py
+++ splatter.py
@@ -1,10 +1,12 @@
 
 import uuid
+from bson.objectid import ObjectId
 
 class SplatService(object):
 
-    def __init__(self, database):
-        self.database = database
+    def __init__(self, db_client):
+        self.db_client = db_client
+        self.database = db_client.get_default_database()
 
     def create_api_key(self, name, key=None):
 
@@ -15,13 +17,25 @@ class SplatService(object):
         result = self.database.APIKeys.insert_one({'name': name, 'key': key})
         return str(result.inserted_id)
 
-    def _construct_optional_query(self, dic):
+    def __construct_optional_query(self, dic, doc_id=None):
         query_filter = {}
         for key, value in dic.items():
             if value is not None:
                 query_filter[key] = value
+        if doc_id is not None:
+            query_filter['_id'] = ObjectId(doc_id)
         return query_filter
 
     def delete_api_key(self, doc_id=None, name=None, key=None):
-        query_filter = self._construct_optional_query({'_id': doc_id, 'name': name, 'key': key})
+        query_filter = self.__construct_optional_query({'name': name, 'key': key}, doc_id)
         return self.database.APIKeys.find_one_and_delete(query_filter)
+
+    def find_api_key(self, doc_id=None, name=None):
+        query_filter = self.__construct_optional_query({'name': name}, doc_id)
+        return self.database.APIKeys.find_one(query_filter)
+
+    def find_all_api_keys(self):
+        return self.database.APIKeys.find()
+
+    def update_api_key(self, doc_id, key=None, name=None):
+        return self.database.APIKeys.find_one_and_update({'_id': ObjectId(doc_id)}, {'$set': {'key': key, 'name': name}})
